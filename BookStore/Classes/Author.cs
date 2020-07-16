a6f0a09a0dac41530e8982b30317e164adb392e4
paulmrest <paulmrest@gmail.com>
2020-07-15 23:10:08 -0700

    Complete implementation - forgot to ACP

---
+++ BookStore/Classes/Author.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookStore.Classes;
+
+namespace BookStore.Classes
+{
+    public class Author
+    {
+     
+        public enum Genders
+        {
+            Other = 1,
+            NonBinary,
+            Female,
+            Male
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int YearBorn { get; set; }
+
+        public Genders Gender { get; set; }
+
+        /// <summary>
+        /// A constructor for a new Author object.
+        /// </summary>
+        /// <param name="firstName">
+        /// string: a first name
+        /// </param>
+        /// <param name="lastName">
+        /// string: a last name
+        /// </param>
+        public Author(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
